68622d9e8749799e39a2a95a9fdc60db2c3fb3fc
Jorge Lira <jorgelira542502@gmail.com>
2021-03-07 15:23:40 -0300

    Projeto

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        400:{
+            items:3
+        },
+        600:{
+            items:6
+        },
+        750:{
+            items:8
+        },
+        1000:{
+            items:10
+        }
+    }
+})
